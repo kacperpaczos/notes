e7af8840e5f7e235d713da79d2d8948896e15fc6
Kacper Paczos <kacper.paczos@linux.pl>
2024-07-12 14:39:28 +0200

    Init

---
+++ se/algorithms-part1/module 2/Dynamic connectivity problem/UnionFind.java
@@ -0,0 +1,24 @@
+public class UnionFind {
+    private int[] parent;  // tablica reprezentująca połączenia
+    private int count;     // liczba rozłącznych zbiorów
+
+    public UnionFind(int n) {
+        // Konstruktor - inicjalizacja struktury
+    }
+
+    // łączy dwa elementy
+    public void union(int p, int q){
+
+    }
+
+    // znajduje reprezentnta dla danego elementu
+    public int find(int p) {
+        return 0;
+    }
+
+    // sprawdza czy dwa elementy są połączone
+    public boolean isConnected(int p, int q){
+        return true;
+    }
+    
+}
